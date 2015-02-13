74a3bd3fd6fcf67ec4c97457c71e2b0e751f6f70
Jomel Imperio <jimperio@gmail.com>
2015-02-13 13:10:05 +0800

    Fix index.html template and access to static/templates folders

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+get_base_dir = lambda dir:  os.path.join(os.path.abspath('../'), dir)
+
+app = Flask(__name__, 
+  static_folder=get_base_dir('static'), 
+  template_folder=get_base_dir('templates'))
 
 @app.route('/')
 def hello():
